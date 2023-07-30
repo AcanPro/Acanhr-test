340bed5dcb3f699a840c98f8ceebf279d5a6acb2
AcanPro <acanpro@qq.com>
2023-07-30 16:06:10 +0800

    调整logo

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,135 +1,134 @@
 <template>
-	<div class="login-container">
-		<div class="logo" />
-		<div class="form">
-			<h1>登录</h1>
-			<el-card shadow="never" class="login-card">
-				<!--登录表单-->
-				<el-form ref="form" :model="loginForm" :rules="loginRules">
-					<el-form-item prop="mobile">
-						<el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input v-model="loginForm.password" placeholder="请输入密码" show-password />
-					</el-form-item>
-					<el-form-item prop="isAgree">
-						<el-checkbox v-model="loginForm.isAgree">
-							用户平台使用协议
-						</el-checkbox>
-					</el-form-item>
-					<el-form-item>
-						<el-button type="primary" style="width: 350px;" @click="login">
-							登录
-						</el-button>
-					</el-form-item>
-				</el-form>
-			</el-card>
-		</div>
-	</div>
+  <div class="login-container">
+    <div class="logo" />
+    <div class="form">
+      <h1>登录</h1>
+      <el-card shadow="never" class="login-card">
+        <!--登录表单-->
+        <el-form ref="form" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" placeholder="请输入密码" show-password />
+          </el-form-item>
+          <el-form-item prop="isAgree">
+            <el-checkbox v-model="loginForm.isAgree">
+              用户平台使用协议
+            </el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" style="width: 350px;" @click="login">
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
+  </div>
 </template>
 <script>
-import { login } from '@/api/user';
 
 export default {
-	name: "Login",
-	data() {
-		return {
-			loginForm: {
-				mobile: process.env.NODE_ENV === 'development' ? '13800000002' : '',
-				password: process.env.NODE_ENV === 'development' ? 'hm#qd@23!' : '',
-				isAgree: false
-			},
-			loginRules: {
-				mobile: [
-					{ required: true, message: '请输入手机号', trigger: 'blur' },
-					{ pattern: /^1[3,5,6,7,8,9]\d{9}$/, message: '手机格式不对', trigger: 'blur' }
-				],
-				password: [
-					{ required: true, message: '请输入密码', trigger: 'blur' },
-					{ min: 6, max: 16, message: '密码长度为6-16位之间', trigger: 'blur' }
-				],
-				isAgree: [
-					{ validator: this.validatePass, trigger: 'change' }]
-			}
-		}
-	},
-	methods: {
-		validatePass(rule, value, callback) {
-			value ? callback() : callback(new Error('你必须勾选用户协议'))
-		},
-		login() {
-			this.$refs.form.validate(async (valid) => {
-				if (valid) {
-					// alert('通过')
-					await this.$store.dispatch('user/loginAction', this.loginForm)
-					this.$router.push('/')
-				}
-			})
-		}
-	}
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        mobile: process.env.NODE_ENV === 'development' ? '13800000002' : '',
+        password: process.env.NODE_ENV === 'development' ? 'hm#qd@23!' : '',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { pattern: /^1[3,5,6,7,8,9]\d{9}$/, message: '手机格式不对', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码长度为6-16位之间', trigger: 'blur' }
+        ],
+        isAgree: [
+          { validator: this.validatePass, trigger: 'change' }]
+      }
+    }
+  },
+  methods: {
+    validatePass(rule, value, callback) {
+      value ? callback() : callback(new Error('你必须勾选用户协议'))
+    },
+    login() {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          // alert('通过')
+          await this.$store.dispatch('user/loginAction', this.loginForm)
+          this.$router.push('/')
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
 .login-container {
-	display: flex;
-	align-items: stretch;
-	height: 100vh;
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
 
-	.logo {
-		flex: 3;
-		background: rgba(38, 72, 176) url(../../assets/common/login_back.png) no-repeat center / cover;
-		border-top-right-radius: 60px;
-		display: flex;
-		flex-direction: column;
-		align-items: flex-end;
-		justify-content: center;
-		padding: 0 100px;
+  .logo {
+    flex: 3;
+    background: rgba(38, 72, 176) url(../../assets/common/login_back.png) no-repeat center / cover;
+    border-top-right-radius: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    padding: 0 100px;
 
-		.icon {
-			background: url(../../assets/common/logo.png) no-repeat 70px center / contain;
-			width: 300px;
-			height: 50px;
-			margin-bottom: 50px;
-		}
+    .icon {
+      background: url(../../assets/common/logo.png) no-repeat 70px center / contain;
+      width: 300px;
+      height: 50px;
+      margin-bottom: 50px;
+    }
 
-		p {
-			color: #fff;
-			font-size: 18px;
-			margin-top: 20px;
-			width: 300px;
-			text-align: center;
-		}
-	}
+    p {
+      color: #fff;
+      font-size: 18px;
+      margin-top: 20px;
+      width: 300px;
+      text-align: center;
+    }
+  }
 
-	.form {
-		flex: 2;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		padding-left: 176px;
+  .form {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 176px;
 
-		.el-card {
-			border: none;
-			padding: 0;
-		}
+    .el-card {
+      border: none;
+      padding: 0;
+    }
 
-		h1 {
-			padding-left: 20px;
-			font-size: 24px;
-		}
+    h1 {
+      padding-left: 20px;
+      font-size: 24px;
+    }
 
-		.el-input {
-			width: 350px;
-			height: 44px;
+    .el-input {
+      width: 350px;
+      height: 44px;
 
-			.el-input__inner {
-				background: #f4f5fb;
-			}
-		}
+      .el-input__inner {
+        background: #f4f5fb;
+      }
+    }
 
-		.el-checkbox {
-			color: #606266;
-		}
-	}
+    .el-checkbox {
+      color: #606266;
+    }
+  }
 }
 </style>
